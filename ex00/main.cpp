047436dfb5e4ef3f02b3a5be0b9ea5d0d2dc9fd1
Ana <lana.ohama@gmail.com>
2025-06-28 19:36:23 -0300

    Create main.cpp

---
+++ ex00/main.cpp
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: apaula-l <apaula-l@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/06/28 19:23:31 by apaula-l          #+#    #+#             */
+/*   Updated: 2025/06/28 19:36:47 by apaula-l         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Zombie.hpp"
+
+int  main()
+{
+  Zombie* heapZombie = newZombie("Heap Zombie");
+  heapZombie->announce();
+
+  randomChump("Stack Zzombie");
+
+  delete heapZombie;
+
+  return 0;
+}
