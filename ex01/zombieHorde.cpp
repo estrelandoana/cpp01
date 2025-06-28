493dc0f572691f54ce5570322a1a7c11d6bf212d
Ana <lana.ohama@gmail.com>
2025-06-28 20:52:37 -0300

    Create zombieHorde.cpp

---
+++ ex01/zombieHorde.cpp
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   zombieHorde.cpp                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: apaula-l <apaula-l@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/06/28 20:25:18 by apaula-l          #+#    #+#             */
+/*   Updated: 2025/06/28 20:38:42 by apaula-l         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Zombie.hpp"
+
+Zombie*  zombieHorde(int N, std::string name)
+{
+  int  i;
+
+  if (N < 1)
+  {
+    std::cout << "Less than 1 zombie it's not a horde, let's infect more people! Muahahahaha" <<std::endl;
+    return NULL;
+  }
+  Zombie* horde = new Zombie[N];
+  for (i = 0; i < N; i++)
+    horde[i].setName(name);
+  return (horde);
+}
