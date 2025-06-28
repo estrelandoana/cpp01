765ba9e73d7f322e8e9cfc21affc3f3cd0ae6948
Ana <lana.ohama@gmail.com>
2025-06-28 20:22:51 -0300

    Create Zombie.hpp

---
+++ ex01/Zombie.hpp
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Zombie.hpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: apaula-l <apaula-l@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/06/28 20:03:36 by apaula-l          #+#    #+#             */
+/*   Updated: 2025/06/28 20:22:28 by apaula-l         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef ZOMBIE_HPP
+# define ZOMBIE_HPP
+# include <iostream>
+# include <string>
+
+class Zombie
+{
+  public:
+    Zombie(void);
+    Zombie(std::string name);
+    ~Zombie(void);
+
+    void announce(void) const;
+    void setName(std::string name);
+
+private:
+    std::string _name;
+};
+
+Zombie*  zombieHorde(int N, std::string name);
+
+#endif
