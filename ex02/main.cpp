5444ea0b34f256413654f44f7be1127a28cacb91
Ana <lana.ohama@gmail.com>
2025-06-30 03:39:43 +0000

    Creating ex02

---
+++ ex02/main.cpp
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: apaula-l <apaula-l@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/06/30 00:07:16 by apaula-l          #+#    #+#             */
+/*   Updated: 2025/06/29 23:47:35 by apaula-l         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include <string>
+
+int main()
+{
+    std::string brain = "HI THIS IS BRAIN";
+    std::string* stringPTR = &brain;
+    std::string& stringREF = brain;
+
+    std::cout << "Memory addresses" << std::endl;
+    std::cout << "Brain's address: " << &brain << std::endl;
+    std::cout << "stringPTR's address: " << stringPTR << std::endl;
+    std::cout << "stringREF's address: " << &stringREF << std::endl;
+
+    std::cout << "\nValue" << std::endl;
+    std::cout << "Brain's value: " << brain << std::endl;
+    std::cout << "stringPTR's value: " << *stringPTR << std::endl;
+    std::cout << "stringREF's value: " << stringREF <<std::endl;
+
+    return 0;
+}
